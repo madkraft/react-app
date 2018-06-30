34d505bed961c20f6033af81a988ba34c6729f48
Anton Kostiuchkov <kostiuchkova@ryanair.com>
2018-06-30 23:09:17 +0200

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+import HomePage from './components/pages/HomePage'
+import AboutPage from './components/pages/AboutPage'
 
-const App = () => <div>Hello World</div>
+const App = () => (
+  <div>
+    <Route path="/" exact component={HomePage} />
+    <Route path="/About" exact component={AboutPage} />
+  </div>
+)
 
 export default App
---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link to="/about">About</Link>
+    </div>
+  )
+}
+
+export default HomePage
